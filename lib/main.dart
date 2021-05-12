e8a28d12d123a3d05cde1138b41391c79fd5a870
niloysikdar <niloysikdar30@gmail.com>
2021-05-12 20:00:08 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:animated_login_screen/screens/create_account.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CreateAccount(),
+    );
+  }
+}
